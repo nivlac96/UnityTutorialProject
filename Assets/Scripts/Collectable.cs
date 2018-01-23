60e802f8d1b2ba4ad77ac9abf185086cb8c279d4
Christopher Paz <cpaz1@crm.ucsc.edu>
2018-01-23 13:49:56 -0800

    the project

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour 
+{
+
+	public AudioSource coin;
+	// Use this for initialization
+	void Start () 
+	{
+		coin = GameObject.FindGameObjectWithTag ("CoinSound").GetComponent<AudioSource> ();
+	}
+	
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.tag.Equals("Player")) 
+		{
+			Debug.Log ("Collected");
+			FindObjectOfType<Counter> ().IncreaseCount ();
+			coin.Play ();
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour 
+{
+
+	int count = 0;
+	int total_collectables;
+	Text counter;
+
+	// Use this for initialization
+	void Start () 
+	{
+		total_collectables = FindObjectsOfType<Collectable>().Length;
+		counter = GetComponentInChildren<Text>();
+	}
+
+	public void IncreaseCount () 
+	{
+		count++;
+		if (count == total_collectables) 
+		{
+			counter.text = "Everything is gone!";
+		} 
+		else 
+		{
+			counter.text = "Collectables: " + count;
+		}
+	}
+}
